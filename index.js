f10bd845cf6447a8de0a455f706f075bbfba64a5
Reid <rlj0713@gmail.com>
2021-08-07 11:49:04 -0400

    fixed createPuzzle submit const

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const puzzlesUrl = "http://localhost:3000/puzzles.json"
 
 const getRandomPuzzleButton = document.getElementById("random-puzzle-form")
-const createPuzzleButton = document.getElementById("puzzle-form")
+const createPuzzleButton = document.getElementById("submit-puzzle")
 
 createPuzzleButton.addEventListener("click", handleSubmit)
 
