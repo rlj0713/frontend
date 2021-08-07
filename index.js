b9f452dd187c90ab0b130135ffe2fe3909b6395c
Reid <rlj0713@gmail.com>
2021-08-07 19:08:02 -0400

    hard-coded uer-input to build scramble function

--- index.js
+++ index.js
@@ -4,11 +4,16 @@ const puzzlesUrl = "http://localhost:3000/puzzles.json"
 const randomPuzzleForm = document.querySelector("#random-puzzle")
 const createPuzzleButton = document.getElementById("submit-puzzle")
 const userSubmittedString = document.querySelector("#puzzle-solutiion").value
+let puzzleSpace = document.getElementById("interactive-puzzle")
+sentence = "Eventually this will be user input!!"
 
 randomPuzzleForm.addEventListener("click", makeCallToApi)
-createPuzzleButton.addEventListener("click", scramble(userSubmittedString))
 
-// This returns an array of objects at the selected difficulty level
+createPuzzleButton.addEventListener('click', scramble(sentence))
+
+
+
+// This returns a random object at the selected difficulty level
 function makeCallToApi() {  
     fetch(puzzlesUrl)
     .then(resp => resp.json())
@@ -22,11 +27,20 @@ function makeCallToApi() {
                 array.push(obj)
             }
         })
-        return array
+        let puzzleObject = array[Math.floor(Math.random() * array.length)];
+        return puzzleObject
     })
 };
 
-// This returns a crypto-valid array of letters given a sentence.
+// This returns a crypto-valid array of letters given an object.
+function makeString(object) {
+    debugger
+}
+
+
+
+
+// This returns a crypto-valid array of letters given a string.
 function scramble(sentence) {
     let alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
     let sentenceArray = []
@@ -45,6 +59,5 @@ function scramble(sentence) {
         scrambledSentence.push(character);
     }
     });
-    //   return scrambledSentence;
-    console.log(scramble(sentence));
+    puzzleSpace.innerText = scrambledSentence
 }
