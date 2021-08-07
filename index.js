505b3b5b470dc4fd7d35baa3544e532e9888e7f4
Reid <rlj0713@gmail.com>
2021-08-07 11:44:09 -0400

    assigned variables to dom buttons

--- index.js
+++ index.js
@@ -1,4 +1,21 @@
 
 const puzzlesUrl = "http://localhost:3000/puzzles.json"
 
-let fetchRequest = fetch(puzzlesUrl)
+const getRandomPuzzleButton = document.getElementById("random-puzzle-form")
+const createPuzzleButton = document.getElementById("puzzle-form")
+
+createPuzzleButton.addEventListener("click", handleSubmit)
+
+function handleSubmit(event) {
+    debugger
+}
+
+
+
+
+
+// getRandomPuzzleButton.addEventListener("click", handleSubmit)
+
+// function handleSubmit(event) {
+//     debugger
+// }
